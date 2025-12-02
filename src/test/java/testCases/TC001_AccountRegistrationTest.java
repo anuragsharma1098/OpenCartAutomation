ff00858d142128445444dbfc599001458a8efb7d
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-12-02 16:17:41 +0530

    Log4j2

--- src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -11,29 +11,50 @@ public class TC001_AccountRegistrationTest extends BaseClass {
 
 	@Test
 	public void verifyRegistration() {
-		// Code to verify account registration
-		HomePage homePage = new HomePage(driver);
-		homePage.clickMyAccount();
-		homePage.clickRegister();
-		AccountRegistrationPage regPage = new AccountRegistrationPage(driver);
-		// regPage.setFirstName("John");
-		regPage.setFirstName(randomString().toUpperCase());
-		// regPage.setLastName("Doe");
-		regPage.setLastName(randomString().toUpperCase());
-		// regPage.setEmail("johndoes_123456@gmail.com");
-		regPage.setEmail(randomString() + "@gmail.com");
-		// regPage.setTelephone("1234567890");
-		regPage.setTelephone(randomNumber());
-		// regPage.setPassword("xyz123456");
-		String pwd = randomAlphaNumeric();
-		regPage.setPassword(pwd);
-		// regPage.setConfirmPassword("xyz123456");
-		regPage.setConfirmPassword(pwd);
-		regPage.checkPrivacyPolicy();
-		regPage.clickContinue();
+		logger.info("***** Starting TC001_AccountRegistrationTest *****");
+		try {
+			// Code to verify account registration
+			HomePage homePage = new HomePage(driver);
+			homePage.clickMyAccount();
+			logger.info("Clicked on My Account link");
+			homePage.clickRegister();
+			logger.info("Clicked on Register link");
+			AccountRegistrationPage regPage = new AccountRegistrationPage(driver);
+			logger.info("Providing customer data");
+			// regPage.setFirstName("John");
+			regPage.setFirstName(randomString().toUpperCase());
+			// regPage.setLastName("Doe");
+			regPage.setLastName(randomString().toUpperCase());
+			// regPage.setEmail("johndoes_123456@gmail.com");
+			regPage.setEmail(randomString() + "@gmail.com");
+			// regPage.setTelephone("1234567890");
+			regPage.setTelephone(randomNumber());
+			// regPage.setPassword("xyz123456");
+			String pwd = randomAlphaNumeric();
+			regPage.setPassword(pwd);
+			// regPage.setConfirmPassword("xyz123456");
+			regPage.setConfirmPassword(pwd);
+			regPage.checkPrivacyPolicy();
+			regPage.clickContinue();
+			logger.info("Clicked on Continue button");
 
-		String confmsg = regPage.getConfirmationMsg();
-		Assert.assertEquals(confmsg, "Your Account Has Been Created!");
+			String confmsg = regPage.getConfirmationMsg();
+			if (confmsg.equals("Your Account Has Been Created!")) {
+				Assert.assertTrue(true);
+				logger.info("Account registration successful");
+			} else {
+				Assert.assertTrue(false);
+				logger.error("Account registration failed");
+				logger.debug("Expected confirmation message not received");
+			}
+			// Assert.assertEquals(confmsg, "Your Account Has Been Created!");
+		} catch (Exception e) {
+			logger.error("Error occurred during registration: " + e.getMessage());
+			logger.debug("Error details: ", e.getMessage());
+			Assert.fail("Test case failed due to exception: " + e.getMessage());
+		}
+
+		logger.info("***** Finished TC001_AccountRegistrationTest *****");
 	}
 
 }
